115fe4089d1b01dea78938a39997afad7bea92c9
dev.jain <dev.jain@zobiwebsolutions.com>
2025-06-13 12:07:09 +0530

    Added Form Control

--- src/app/task/task.ts
+++ src/app/task/task.ts
@@ -1,36 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TaskService } from './services/task.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TaskModel } from './models/task.model';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './task.html',
   styleUrl: './task.css'
 })
-export class Task {
+export class Task implements OnInit {
+  taskForm!: FormGroup; 
   tasks: TaskModel[] = [];
-  taskTitle: string = '';
-  constructor(private taskService: TaskService){  }
+  
+  constructor(private fb: FormBuilder, private taskService: TaskService){  }
 
   ngOnInit() {    
+    this.taskForm = this.fb.group({
+        title: ['', [Validators.required, Validators.minLength(3)]]
+    })
+
+    this.loadTasks();
+  }
+
+  loadTasks(): void{
     this.tasks = this.taskService.getTasks();
   }
 
   onSubmit(){
-    this.taskService.addTask(this.taskTitle);
-    this.taskTitle = '';
+    if(this.taskForm.invalid) return;
+    const title = this.taskForm.value.title;
+    this.taskService.addTask(title);
+    this.taskForm.reset();
+    this.loadTasks();
   }
 
   toggleStatus(id: number){
     this.taskService.toggleTaskStatus(id);
+    this.loadTasks();
   }
 
   deleteTask(id: number){
     this.taskService.deleteTask(id);
-    this.tasks = this.taskService.getTasks();
+    this.loadTasks();
   }
 }
